f81be3464f7c648a38313a446f71ec927d1c6761
Viji7789 <vijinatarajan7789@gmail.com>
2022-09-22 09:55:53 -0400

    first commit

---
+++ src/test/java/com/pages/Demo_page.java
@@ -0,0 +1,8 @@
+package com.pages;
+
+public class Demo_page {
+
+	public void demo() {
+		System.out.println("this is test git demo page object");
+	}
+}
---
+++ src/test/java/com/testcases/Git_testcase.java
@@ -0,0 +1,8 @@
+package com.testcases;
+
+public class Git_testcase {
+
+	public void Git_test() {
+		System.out.println("this is sample push test case");
+	}
+}
